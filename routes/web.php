a876f554863cd7fa33bac04271da1112fab97a9e
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-14 11:23:05 +0530

    added routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
-});
+})->name('welcome');
+Route::get('/', function () {
+    return view('Login');
+})->name('login');
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
