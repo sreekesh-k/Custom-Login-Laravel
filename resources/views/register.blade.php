5963bc8f47f12c0804898b9004cfa461206cb957
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-14 12:40:39 +0530

    error and success

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,6 +2,14 @@
 @section('title', 'Register')
 @section('content')
     <div class="container">
+        <div class='mt-5'>
+            @if (session()->has('Error'))
+                <div class="alert alert-danger">{{ session('Error') }}</div>
+            @endif
+            @if (session()->has('Success'))
+                <div class="alert alert-success">{{ session('Success') }}</div>
+            @endif
+        </div>
         <form action="{{ route('register.post') }}" method="POST" class="ms-auto me-auto" style="width:500px">
             @csrf
             <div class="mb-3">
