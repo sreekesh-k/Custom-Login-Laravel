d4875c699eee0455495e115d52d62d8cfb3df50c
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-14 12:27:04 +0530

    route fix

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,5 @@ Route::get('/', [AuthManager::class, 'login'])->name('login');
 Route::post('/', [AuthManager::class, 'loginPost'])->name('login.post');
 
 Route::get('/register', [AuthManager::class, 'register'])->name('register');
-Route::post('/', [AuthManager::class, 'registerPost'])->name('register.post');
+Route::post('/register', [AuthManager::class, 'registerPost'])->name('register.post');
 Route::get('/logout', [AuthManager::class, 'logout'])->name('logout');
