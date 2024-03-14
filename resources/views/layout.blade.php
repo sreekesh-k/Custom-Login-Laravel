5da011e2bbda0ff4ae073e1a2e6229bab26fb985
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-14 11:36:03 +0530

    header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
 </head>
 
 <body>
+    @include('include.header')
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
